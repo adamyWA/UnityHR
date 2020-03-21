5a8f1b6885345f3eee65a183f903d7f015ec20c6
adamywa <bizarrotx@gmail.com>
2020-03-21 02:23:14 -0700

    basically a full rewrite for more better

---
+++ Listeners/EmployeeListener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+public class EmployeeListener : MonoBehaviour
+{
+    public Employee Employee;
+    public Staff Staff;
+
+    private void Start()
+    {
+        Employee = gameObject.GetComponent<Employee>(); //game may contain multiple employees, only get the employee attached to the same gameobject
+
+        Employee.Quitted += HandleOnQuitted;
+        Employee.CalledOut += HandleOnCalledOut;
+        Employee.Covered += HandleOnCovered;
+    }
+    void HandleOnCovered(OpenShift shift)
+    {
+        Debug.Log(Employee.Name + " covered " + shift.Day + " " + shift.Shift);
+    }
+    void HandleOnQuitted(Schedule sched, Staff staff)
+    {
+        if(staff.Employees.Contains(Employee))
+        {
+            staff.Employees.Remove(Employee);
+        }
+        foreach(var shift in Employee.WorkShifts)
+        {
+            sched.OpenShift(shift);
+        }
+        Employee.Employed = false;
+        Employee.HasOpenShifts = true;
+    }
+    void HandleOnCalledOut(Schedule sched, OpenShift shift)
+    {
+        if(!Employee.OpenShifts.Contains(shift)) //make sure the employee is working this shift
+        {
+            Employee.OpenShifts.Add(shift);
+            sched.OpenShift(shift);
+            Employee.HasOpenShifts = true;
+        }
+    }
+}
---
+++ Publishers/Staff.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour {
+
+    public bool HasEmployees;
+    public bool Full;
+    public int Max { get; set; }
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public List<Employee> Employees;
+
+    public event Action<Employee> Hired;
+    public event Action<Employee> Fired;
+
+	// Use this for initialization
+	void Start () {
+        #region Initialize
+        HasEmployees = false;
+        Full = false;
+        Max = 10;
+        Name = "Default Staff";
+        Id = 1337;
+        Employees = new List<Employee>();
+        #endregion
+    }
+
+    public void Hire(Employee emp)
+    {
+        if (Hired != null)
+        {
+            if (!Employees.Contains(emp) && Employees.Count < Max)
+            {
+                Employees.Add(emp);
+                if(Employees.Count >= Max)
+                    Full = true;
+
+                Hired(emp);
+            }
+        }
+    }
+    public void Fire(Employee emp)
+    {
+        if (Fired != null)
+        {   
+            if(Employees.Contains(emp))
+            {
+                Employees.Remove(emp);
+                if (Employees.Count <= Max)
+                    Full = false;
+
+                Fired(emp);
+            }
+            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
